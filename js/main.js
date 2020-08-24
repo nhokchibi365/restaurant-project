d0750852dac741de13c8667311d7fb5a56868ad5
nhokchibi365 <62950577+nhokchibi365@users.noreply.github.com>
2020-08-24 18:20:52 +0700

    Update var 1.5
    Big update and fix bug

--- js/main.js
+++ js/main.js
@@ -1,70 +1,84 @@
-    /* Scroll smooth */
-    $(document).ready(function () {
-        // Add smooth scrolling to all links
-        $("a").on('click', function (event) {
-
-            // Make sure this.hash has a value before overriding default behavior
-            if (this.hash !== "") {
-                // Prevent default anchor click behavior
-                event.preventDefault();
-
-                // Store hash
-                var hash = this.hash;
-
-                // Using jQuery's animate() method to add smooth page scroll
-                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top
-                }, 800, function () {
-
-                    // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = hash;
-                });
-            } // End if
-        });
-    });
-
-    /* Go to top */
-    var mybutton = document.getElementById("go-to-top"); // get the button
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-        scrollFunction()
-    };
-
-    function scrollFunction() {
-        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-            mybutton.style.display = "block";
-        } else {
-            mybutton.style.display = "none";
-        }
+/* ..............................................
+        Map Full
+    ............................................... */
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 100) {
+      $("#go-to-top").fadeIn();
+    } else {
+      $("#go-to-top").fadeOut();
     }
+  });
+  $("#go-to-top").click(function () {
+    $("html, body").animate({
+        scrollTop: 0,
+      },
+      1500
+    );
+    return false;
+  });
+});
 
-    /* animation */
-    AOS.init({
-        offset: 200,
-        delay: 0,
-        duration: 1000
-    });
+/* ..............................................
+        Special Menu
+............................................... */
+(function () {
+  var current = $("li > a.active");
+  $("li > a").click(function () {
+    current.removeClass("active");
+    current = $(this);
+    current.addClass("active");
+  });
+})();
+var $root = $("html, body");
 
-    /* Carousel */
-    $(document).ready(function () {
-        $('.qoute').slick({
-            slidesToShow: 1,
-            dots: true,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 3500,
-        });
-    });
-    $(document).ready(function () {
-        $('.reviews').slick({
-            slidesToShow: 1,
-            dots: true,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 3500,
-        });
-    });
+$('a[href^="#"]').click(function () {
+  var href = $.attr(this, "href");
 
-    /* BaguetteBox */
-    baguetteBox.run('.gallery');
+  $root.animate({
+      scrollTop: $(href).offset().top,
+    },
+    1000,
+    function () {
+      window.location.hash = href;
+    }
+  );
+
+  return false;
+});
+/* ..............................................
+        Animation
+    ............................................... */
+AOS.init({
+  offset: 200,
+  delay: 0,
+  duration: 1000,
+});
+
+/* ..............................................
+        Carousel (Qoute, Customer Reivews)
+    ............................................... */
+$(document).ready(function () {
+  $(".qoute").slick({
+    slidesToShow: 1,
+    dots: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    arrows: false,
+  });
+});
+$(document).ready(function () {
+  $(".reviews").slick({
+    slidesToShow: 1,
+    dots: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+  });
+});
+
+/* ..............................................
+        BaguetteBox
+    ............................................... */
+baguetteBox.run(".gallery");
